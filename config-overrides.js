d97d2449691ad49a4fb55e6c9860171628999acb
wuqiangnan <2441277210@qq.com>
2023-01-31 21:49:35 +0800

    chore: add commitlint

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const { override, addWebpackAlias } = require("customize-cra");
+const path = require("path");
+
+module.exports = override(
+  addWebpackAlias({
+    "@": path.resolve(__dirname, "src"),
+    "@components": path.resolve(__dirname, "src/components"),
+    "@helpers": path.resolve(__dirname, "src/helpers"),
+    "@hooks": path.resolve(__dirname, "src/hooks"),
+  })
+);
